ffb960cf86f2722c6468382da7c8cb8ba6857997
Manch Hon <5395258+manchung@users.noreply.github.com>
2023-12-17 21:30:46 -0800

    Fixed security issues. Installed Helmet, Mongo-Sanitize and Sanitize-Html packages.

--- schemas.js
+++ schemas.js
@@ -1,12 +1,35 @@
-const Joi = require('joi');
+const BaseJoi = require('joi');
+const sanitizeHtml = require('sanitize-html');
+
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHtml': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHtml: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHtml', {value})
+                return clean;
+            }
+        }
+    }
+});
+
+const Joi = BaseJoi.extend(extension);
 
 module.exports.campgroundSchema = new Joi.object({
     campground: Joi.object({
-        title: Joi.string().required(),
+        title: Joi.string().required().escapeHtml(),
         price: Joi.number().required().min(0),
         // image: Joi.string().required(),
-        location: Joi.string().required(),
-        description: Joi.string().required()
+        location: Joi.string().required().escapeHtml(),
+        description: Joi.string().required().escapeHtml()
     }).required(),
     deleteImages: Joi.array()
 });
@@ -14,7 +37,7 @@ module.exports.campgroundSchema = new Joi.object({
 module.exports.reviewSchema = new Joi.object({
     review: Joi.object({
         rating: Joi.number().required().min(1).max(5),
-        body: Joi.string().required()
+        body: Joi.string().required().escapeHtml()
     }).required()
 });
 
